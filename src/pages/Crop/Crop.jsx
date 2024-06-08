f0f2c939cff5c84890c42e186aa44d84401fa695
Eldho Paulose <eldhopaulose0485@gmail.com>
2024-06-08 13:05:08 +0530

    crop page completed

--- src/pages/Crop/Crop.jsx
+++ src/pages/Crop/Crop.jsx
@@ -11,8 +11,6 @@ import Gold from '../../assets/gold.jpg';
 import Champagne from '../../assets/f7e7ce.jpg';
 import White from '../../assets/white.jpg';
 
-
-
 function Crop() {
     const [frameColor, setFrameColor] = useState('black');
     const [orientation, setOrientation] = useState('portrait');
@@ -20,11 +18,10 @@ function Crop() {
     const [price, setPrice] = useState(70);
     const [customPrice, setCustomPrice] = useState(110);
     const [selectedSize, setSelectedSize] = useState('30x40 cm');
-    const [tintedImage, setTintedImage] = useState(null); // New state for tinted image
+    const [tintedImage, setTintedImage] = useState(null);
     const imageRef = useRef(null);
     const cropperRef = useRef(null);
     const [selectedFile, setSelectedFile] = useState(null);
-
 
     const getImageName = (color) => {
         switch (color) {
@@ -55,6 +52,20 @@ function Crop() {
         '120x180 cm': 270
     };
 
+    const sizeDimensionsMap = {
+        '30x40 cm': { portrait: { width: 360, height: 480 }, landscape: { width: 480, height: 360 } },
+        '40x60 cm': { portrait: { width: 400, height: 600 }, landscape: { width: 600, height: 400 } },
+        '50x60 cm': { portrait: { width: 500, height: 600 }, landscape: { width: 600, height: 500 } },
+        '50x70 cm': { portrait: { width: 400, height: 560 }, landscape: { width: 560, height: 400 } },
+        '60x90 cm': { portrait: { width: 360, height: 540 }, landscape: { width: 540, height: 360 } },
+        '60x120 cm': { portrait: { width: 300, height: 600 }, landscape: { width: 600, height: 300 } },
+        '100x200 cm': { portrait: { width: 250, height: 500 }, landscape: { width: 500, height: 250 } },
+        '80x120 cm': { portrait: { width: 400, height: 600 }, landscape: { width: 600, height: 400 } },
+        '70x140 cm': { portrait: { width: 300, height: 600 }, landscape: { width: 600, height: 300 } },
+        '90x120 cm': { portrait: { width: 450, height: 600 }, landscape: { width: 600, height: 450 } },
+        '120x180 cm': { portrait: { width: 360, height: 540 }, landscape: { width: 540, height: 360 } }
+    };
+
     useEffect(() => {
         if (imageSrc && imageRef.current) {
             cropperRef.current = new Cropper(imageRef.current, {
@@ -77,42 +88,22 @@ function Crop() {
             const reader = new FileReader();
             reader.onload = () => setImageSrc(reader.result);
             reader.readAsDataURL(file);
-            setSelectedFile(file); // Update the selectedFile state
+            setSelectedFile(file);
         }
     };
-
 
     const handleFrameChange = (e) => {
         const color = e.target.dataset.color;
         setFrameColor(color);
-        applyTint(color); // Call the applyTint function
+        applyTint(color);
     };
 
     const handleOrientationChange = (e) => {
         const newOrientation = e.target.checked ? 'landscape' : 'portrait';
         setOrientation(newOrientation);
-    
-        // Define default sizes for portrait and landscape modes
-        const portraitWidth = 333.333;
-        const portraitHeight = 500;
-        const landscapeWidth = 500;
-        const landscapeHeight = 333.333;
-    
-        // Set the width and height of the outer box
-        const outerBox = document.getElementById('outerBox');
-        const width = newOrientation === 'landscape' ? landscapeWidth : portraitWidth;
-        const height = newOrientation === 'landscape' ? landscapeHeight : portraitHeight;
-    
-        if (outerBox) {
-            outerBox.style.width = `${width}px`;
-            outerBox.style.height = `${height}px`;
-        }
-    
+        const { width, height } = sizeDimensionsMap[selectedSize][newOrientation];
         resizeBoxes(width, height);
     };
-    
-
-
 
     const resizeBoxes = (width, height) => {
         const outerBox = document.getElementById('outerBox');
@@ -121,7 +112,6 @@ function Crop() {
             outerBox.style.height = `${height}px`;
         }
     };
-
 
     const customCropImage = () => {
         if (cropperRef.current) {
@@ -136,75 +126,12 @@ function Crop() {
         }
     };
 
-
     const handleSizeSelection = (size) => {
-        console.log("Button clicked:", size);
         setSelectedSize(size);
         setPrice(sizePriceMap[size]);
-        const ratio = size.split(' ')[0]; // Extracting the ratio from the size string
-    
-        try {
-            // Define default ratios for portrait and landscape modes
-            let width, height;
-    
-            switch (size) {
-                case '30x40 cm':
-                    width = 360;
-                    height = 480;
-                    break;
-                case '40x60 cm':
-                    width = 400;
-                    height = 600;
-                    break;
-                case '50x60 cm':
-                    width = 500;
-                    height = 600;
-                    break;
-                case '50x70 cm':
-                    width = 400;
-                    height = 560;
-                    break;
-                case '60x90 cm':
-                    width = 540;
-                    height = 360;
-                    break;
-                case '60x120 cm':
-                    width = 600;
-                    height = 300;
-                    break;
-                case '100x200 cm':
-                    width = 500;
-                    height = 250;
-                    break;
-                case '80x120 cm':
-                    width = 400;
-                    height = 600;
-                    break;
-                case '70x140 cm':
-                    width = 600;
-                    height = 300;
-                    break;
-                case '90x120 cm':
-                    width = 600;
-                    height = 450;
-                    break;
-                case '120x180 cm':
-                    width = 540;
-                    height = 360;
-                    break;
-                default:
-                    throw new Error('Invalid size');
-            }
-    
-            resizeBoxes(width, height, orientation);
-        } catch (error) {
-            console.error('Error occurred while handling size selection:', error);
-        }
+        const { width, height } = sizeDimensionsMap[size][orientation];
+        resizeBoxes(width, height);
     };
-    
-
-
-
 
     const handleFrameSelection = (withFrame) => {
         const basePrice = sizePriceMap[selectedSize];
@@ -217,7 +144,7 @@ function Crop() {
             const fabricCanvas = new Fabric.StaticCanvas(canvas);
             const tintFilter = new Fabric.Image.filters.Tint({
                 color: color,
-                opacity: 0.3, // Adjust the opacity as needed
+                opacity: 0.3,
             });
 
             fabricCanvas.backgroundImage.filters.push(tintFilter);
@@ -232,10 +159,10 @@ function Crop() {
                 <div className="w-full md:w-2/3 p-2">
                     <div
                         id="outerBox"
-                        className={`rounded shadow-md transition-all duration-300 `}
+                        className="rounded shadow-md transition-all duration-300"
                         style={{
-                            width: '333.333px',
-                            height: '500px',
+                            width: '360px',
+                            height: '480px',
                             padding: '10px',
                             backgroundImage: `url(golden.webp)`,
                             backgroundColor: frameColor,
@@ -252,10 +179,59 @@ function Crop() {
                         ></div>
                     </div>
                 </div>
+
+                <div className="mt-4 md:hidden lg:hidden">
+                    <div className="flex flex-wrap">
+                        <div className="md:w-full p-2">
+                            <div className="flex">
+                                <label
+                                    htmlFor="customImageUpload"
+                                    className="bg-black text-white px-4 py-2 rounded-l-md cursor-pointer"
+                                >
+                                    Upload File
+                                </label>
+                                <div className="flex-grow bg-gray-200 px-4 py-2 rounded-r-md">
+                                    <input
+                                        type="file"
+                                        id="customImageUpload"
+                                        accept="image/*"
+                                        className="hidden"
+                                        onChange={handleImageUpload}
+                                    />
+                                    <span className="text-gray-500">
+                                        {selectedFile ? selectedFile.name : 'No file selected'}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full md:w-2/3 p-2">
+                            <div className="rounded shadow-md">
+                                {imageSrc && (
+                                    <img
+                                        ref={imageRef}
+                                        id="customInnerBox"
+                                        className="rounded frame max-w-full max-h-full"
+                                        src={imageSrc}
+                                        alt="Custom Crop"
+                                    />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                    {selectedFile ? (
+                        <Button
+                            className="btn btn-secondary button-margin custom-crop-button mt-4"
+                            onClick={customCropImage}
+                        >
+                            Custom Crop
+                        </Button>
+                    ) : null}
+                </div>
+
                 <div className="w-full md:w-1/3 p-2">
                     <div className="mb-4">
                         <h3>Select Frame Colour</h3>
-                        {['black', 'gold', '#f7e7ce', 'white',].map((color) => (
+                        {['black', 'gold', '#f7e7ce', 'white'].map((color) => (
                             <label key={color} className="inline-block mr-2">
                                 <input
                                     type="radio"
@@ -277,45 +253,36 @@ function Crop() {
                     <div className="Orientation-Mode mb-4">
                         <h5>Select Orientation</h5>
                         <div className="flex">
-                            <FaPortrait id="portrait-icon"
+                            <FaPortrait
                                 className={`fa-solid fa-image-portrait ml-2 ${orientation === 'portrait' ? 'text-2xl' : 'text-xl'}`}
-                                title="Portrait" />
+                                title="Portrait"
+                            />
                             <label className="switch ml-3">
-                                <Switch id="flexSwitchCheckDefault" onChange={handleOrientationChange} />
+                                <Switch onChange={handleOrientationChange} />
                                 <span className="slider round"></span>
                             </label>
                             <MdLandscape
-                                id="landscape-icon"
                                 className={`fa-solid fa-image ml-5 ${orientation === 'landscape' ? 'text-2xl' : 'text-xl'}`}
-                                title="Landscape" />
+                                title="Landscape"
+                            />
                         </div>
-                        <div className="mt-4">
-                            <p>Price: {price} د.إ</p>
-                            <p>Custom Price: {customPrice} د.إ</p>
+
+                        <div className="flex">
+                            <div className="mt-4">
+                                <p>Price: {price} د.إ</p>
+                                <p>Custom Price: {customPrice} د.إ</p>
+                            </div>
+                            <Button className="btn btn-secondary button-margin custom-crop-button mt-4">Add</Button>
                         </div>
                     </div>
+
                     <div className="mt-1">
                         <h5>Select Size Ratio</h5>
-                        {[
-                            { size: '30x40 cm', w: 360, h: 480 },
-                            { size: '40x60 cm', w: 400, h: 600 },
-                            { size: '50x60 cm', w: 500, h: 600 },
-                            { size: '50x70 cm', w: 400, h: 560 },
-                            { size: '60x90 cm', w: 360, h: 540 },
-                            { size: '60x120 cm', w: 300, h: 600 },
-                            { size: '100x200 cm', w: 250, h: 500 },
-                            { size: '80x120 cm', w: 400, h: 600 },
-                            { size: '70x140 cm', w: 300, h: 600 },
-                            { size: '90x120 cm', w: 450, h: 600 },
-                            { size: '120x180 cm', w: 360, h: 540 }
-                        ].map(({ size, w, h }) => (
+                        {Object.keys(sizeDimensionsMap).map((size) => (
                             <Button
                                 key={size}
-                                className="mt-3  mr-4 h-16 w-36 md:h-14 md:w-28 lg:h-14 lg:w-28" // Add margin-right to create space between buttons
-                                onClick={() => {
-                                    resizeBoxes(w, h, orientation);
-                                    handleSizeSelection(size);
-                                }}
+                                className="mt-3 mr-4 h-16 w-36 md:h-11 md:w-20 lg:h-11 lg:w-28"
+                                onClick={() => handleSizeSelection(size)}
                             >
                                 {size}
                             </Button>
@@ -324,31 +291,27 @@ function Crop() {
                 </div>
             </div>
 
-            <div className="mt-4">
+            <div className="mt-4 hidden md:block lg:block">
                 <div className="flex flex-wrap">
-                    <div className=" md:w-full  p-2">
-                        <div className="">
-                            <div className="">
-                                <div className="w-full flex items-center md:ml-0 lg:ml-0 ml-12 ">
-                                    <label
-                                        htmlFor="customImageUpload"
-                                        className="bg-black text-white px-4 py-2 rounded-l-md cursor-pointer"
-                                    >
-                                        Upload File
-                                    </label>
-                                    <div className="flex-grow bg-gray-200 px-4 py-2 rounded-r-md">
-                                        <input
-                                            type="file"
-                                            id="customImageUpload"
-                                            accept="image/*"
-                                            className="hidden"
-                                            onChange={handleImageUpload}
-                                        />
-                                        <span className="text-gray-500">
-                                            {selectedFile ? selectedFile.name : 'No file selected'}
-                                        </span>
-                                    </div>
-                                </div>
+                    <div className="md:w-full p-2">
+                        <div className="flex">
+                            <label
+                                htmlFor="customImageUpload"
+                                className="bg-black text-white px-4 py-2 rounded-l-md cursor-pointer"
+                            >
+                                Upload File
+                            </label>
+                            <div className="flex-grow bg-gray-200 px-4 py-2 rounded-r-md">
+                                <input
+                                    type="file"
+                                    id="customImageUpload"
+                                    accept="image/*"
+                                    className="hidden"
+                                    onChange={handleImageUpload}
+                                />
+                                <span className="text-gray-500">
+                                    {selectedFile ? selectedFile.name : 'No file selected'}
+                                </span>
                             </div>
                         </div>
                     </div>
@@ -365,7 +328,6 @@ function Crop() {
                             )}
                         </div>
                     </div>
-
                 </div>
                 {selectedFile ? (
                     <Button
