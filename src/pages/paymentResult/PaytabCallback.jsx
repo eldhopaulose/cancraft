209131999c757c6db88373fb7527125967e9d835
Nasarsoft <nasarsoft@gmail.com>
2024-09-29 16:01:07 +0530

    new

---
+++ src/pages/paymentResult/PaytabCallback.jsx
@@ -0,0 +1,13 @@
+import  { useEffect, useState } from "react";
+const PaytabCallback = () => {
+  
+
+  return (
+    <div className="grid grid-cols-1 grid-rows gap-4">
+        <h4 className="text-3xl font-bold flex justify-center mt-10 mb-10">My Orders</h4>
+       
+    </div>
+);
+};
+
+export default PaytabCallback;
