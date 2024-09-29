e8600df4cb1cc893684f278c0c0f5132a518efc0
Nasarsoft <nasarsoft@gmail.com>
2024-09-29 16:10:34 +0530

    new

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,10 +1,10 @@
 // export const BASE_URL = "https://cancraft.onrender.com/api";
-export const BASE_URL = "http://localhost:3000/api";
-export const DEV_URL = "http://localhost:3000/api";
+// export const BASE_URL = "http://localhost:3000/api";
+// export const DEV_URL = "http://localhost:3000/api";
 
 export const PAYMENTLINK="https://cancraft-admin-panel-backend-6smr.onrender.com/api/payment/paytabs-return"
 
-// export const BASE_URL = "https://cancraft-admin-panel-backend-6smr.onrender.com/api";
-// export const DEV_URL = "https://cancraft-admin-panel-backend-6smr.onrender.com/api";
+export const BASE_URL = "https://cancraft-admin-panel-backend-6smr.onrender.com/api";
+export const DEV_URL = "https://cancraft-admin-panel-backend-6smr.onrender.com/api";
  
 
